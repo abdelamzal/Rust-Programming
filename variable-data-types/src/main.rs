22d1c0d6d91e990372cd6aba0d51bef8a80bfdc1
Amzal abdel <am.amzal@gmail.com>
2020-10-23 17:44:25 +0200

    Variables

---
+++ variable-data-types/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    //integers
+    let x = 45; //i32 integers par defaut
+    let y: i64 = 45; //i64
+    let z: u64 = 45; //u64 unsigned integers : nombres positifs 
+
+    //floating
+    let f = 6.7; //f32
+    let ff: f64 = 6.7; //f64
+
+    //boolean
+    let b: bool = true;
+
+}
---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let y = 30;
+    let mut x = 45;
+
+    println!("La valeur de y est {} ", y);
+    println!("La valeur de x est {} ", x);
+
+    x = 60;
+    println!("La valeur de y ne peut pas être changée");
+    println!("La valeur de x est {} ", x);
+}
